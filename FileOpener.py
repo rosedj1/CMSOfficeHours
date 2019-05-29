4cac6ac8962d37d2be5cf2a4243d134bcd51a260
Cristofer Alexander Caballeros <cris.caballeros@melrose.ihepa.ufl.edu>
2019-05-29 14:50:09 -0400

    Pushing

--- FileOpener.py
+++ FileOpener.py
@@ -2,34 +2,8 @@ from ROOT import *
 import numpy as np
 
 #Class to open a file using TFile and looks what is inside
-def LeafOpener(handle,filename):
-	handle = TFile.Open(filename)
-	handle.ls()	
+def LeafOpener(function):
+	return function**2
 
-	#name = handle.GetClass("TTree").GetName()
-	#print(name)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	#TreeData = handle.GetListOfKeys().GetObjectRef("TTree")
-	#print (TreeData)
-
+y = LeafOpener(2)
+print (y)	
